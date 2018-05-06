891243d2562946073c824560f21f5fcad965ae0b
gonzaloeats <gonzaloescobar2000@gmail.com>
2018-05-05 20:45:08 -0400

    added redshift to copy script

--- copy_to_correct_location.sh
+++ copy_to_correct_location.sh
@@ -3,5 +3,6 @@ sudo echo '225' > /sys/devices/platform/i8042/serio1/serio2/speed
 cp .vimrc ~/.vimrc
 cp .bashrc ~/.bashrc
 cp .inputrc ~/.inputrc
+cp redshift.conf ~/.config/redshift.conf
 mkdir -p ~/.vim/colors
 cp monokai.vim ~/.vim/colors/monokai.vim
